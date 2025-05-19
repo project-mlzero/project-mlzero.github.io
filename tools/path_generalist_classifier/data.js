4f3892fd5a37ce5b21a103478a5945df38ce2105
“FANGAreNotGnu” <kevinfang97@gmail.com>
2025-05-19 22:39:45 +0000

    init

---
+++ tools/path_generalist_classifier/data.js
@@ -0,0 +1,4 @@
+const global_tool_name = "🔬 Pathology Classifier";
+const global_tool_description = "Classify H&E-stained pathology images into one of the given options.";
+
+export { global_tool_name, global_tool_description };
